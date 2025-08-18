6b0df3e1db4248b11bc0476487c531264e301159
Margo <109484645+TyamanovaMargo@users.noreply.github.com>
2025-08-18 15:25:27 +0300

    add config_example.py

---
+++ config_example.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class Config:
+    BRIGHT_DATA_API_TOKEN = os.getenv("BRIGHT_DATA_API_TOKEN", "your key here")
+    BRIGHT_DATA_DATASET_ID = "gd_lwxmeb2u1cniijd7t4"
+    OUTPUT_DIR = "output/"
+    MAX_CONCURRENT_VALIDATIONS = 3
+    VALIDATION_DELAY_MIN = 1.5
+    VALIDATION_DELAY_MAX = 3.5
+    MAX_SNAPSHOT_WAIT = 600
